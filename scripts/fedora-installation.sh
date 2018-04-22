366d82b7d83b5cab3689e5fbca6bbb7d77dcd41b
Charles-Antoine Couret <renault@fedoraproject.org>
2018-04-22 23:52:17 +0200

    Update Fedora post install script

--- scripts/fedora-installation.sh
+++ scripts/fedora-installation.sh
@@ -31,21 +31,21 @@ dnf -y upgrade
 
 ## Base
 dnf -y groupinstall "Fedora Workstation"
-dnf -y install dnf-plugin-system-upgrade deja-dup vlc mplayer mplayer-gui mencoder thunderbird\* lame gstreamer\*-plugin*\ gstreamer1-libav gstreamer1-vaapi ffmpeg pipewire gpm xvidcore empathy transmission-gtk --exclude=\*docs $NO_DEVEL $SKIP_MISSING_PACKAGE
+dnf -y install dnf-plugin-system-upgrade deja-dup vlc mpv mplayer mplayer-gui mencoder thunderbird\* lame gstreamer\*-plugin*\ gstreamer1-libav gstreamer1-vaapi gstreamer-ffmpeg ffmpeg pipewire gpm xvidcore empathy transmission-gtk --exclude=\*docs $NO_DEVEL $SKIP_MISSING_PACKAGE
 
 ## GNOME
 dnf -y groupinstall "Environnement de bureau standard"
 dnf -y install gnome-tweak-tool gnome-weather gitg epiphany swell-foop hitori tali iagno lightsoff quadrapassel four-in-a-row gnome-tetravex gnome-nibbles gnome-taquin gnome-mahjongg gnome-robots gnome-sudoku five-or-more gnome-maps gnome-chess gnome-mines gnome-clocks gnome-boxes polari gnome-music gnome-photos ghex ekiga gnome-tweak-tool gnome-hearts simple-scan gnome-mime-data dconf-editor \*gtkspell\* pygtksourceview\* gnome-doc-utils control-center\* evince\* gnome-user\* gnome-system\* guake gnome-activity-journal gnome-power-manager verbiste-gnome gtg gnome-2048 gnome-klotski gnome-battery-bench gnome-builder gnome-common gnome-dictionary tracker-preferences gedit-plugin* gedit-code-assistance gnome-sound-recorder gnome-todo gnome-themes-legacy gnome-shell-extension-auto-move-windows gnome-shell-extension-activities-configurator gnome-shell-extension-freon gnome-shell-theme-* gnome-multi-writer $NO_DEVEL $SKIP_MISSING_PACKAGE
 
 ## Hardware
-dnf -y install beignet libclc vulkan libva-utils vdpauinfo libva-vdpau-driver libva-intel-driver hplip-gui pcsc-lite-devel libproxy-devel libcurl-devel
+dnf -y install beignet libclc vulkan libva-utils vdpauinfo libva-vdpau-driver libvdpau-va-gl mesa-vdpau-drivers libva-intel-driver hplip-gui pcsc-lite-devel libproxy-devel libcurl-devel
 
 ## Office
-dnf -y install libreoffice-calc libreoffice-writer libreoffice-impress libreoffice-math libreoffice-base libreoffice-draw libreoffice-pdfimport kchmviewer chm2pdf gimp dia $SKIP_MISSING_PACKAGE
+dnf -y install pandoc pandoc-pdf libreoffice-calc libreoffice-writer libreoffice-impress libreoffice-math libreoffice-base libreoffice-draw libreoffice-pdfimport kchmviewer chm2pdf gimp dia $SKIP_MISSING_PACKAGE
 
 ## Development
 dnf -y groupinstall "Outils de développement et bibliothèques pour C" "Outils système"
-dnf -y install gcc gcc-c++ wget nasm subversion git-gui git-tools git-email openssl-devel ncurses-devel ncurses-compat-libs glibc-devel linuxdoc\* kernel-tools kernel-doc kernel-devel kernel-headers python3-sphinx rst2pdf i2c-tools minicom picocom $SKIP_MISSING_PACKAGE
+dnf -y install gcc gcc-c++ cmake wget nasm subversion git-gui git-tools git-email openssl-devel ncurses-devel ncurses-compat-libs glibc-devel linuxdoc\* kernel-tools kernel-doc kernel-devel kernel-headers python3-sphinx rst2pdf i2c-tools minicom picocom paho-c-devel paho-c libmodbus-devel libmodbus $SKIP_MISSING_PACKAGE
 
 ## Buildroot / ptxdist
 dnf -y install doxygen vim-common texinfo makeinfo yacc bison flex automake aclocal autoconf glibc{,-devel}.i686 $NO_DEVEL $SKIP_MISSING_PACKAGE
@@ -63,9 +63,7 @@ dnf -y install qt5-linguist qt5-designer qt-creator qt5-*-devel qt-creator-trans
 dnf -y install gajim easytag youtube-dl policycoreutils-gui blivet-gui soundconverter GraphicsMagick theora-tools crack john freetype-freeworld man-pages dejavu\* acpi acpid tor acpitool screen system-config-\* linux_logo fedora-business-cards fedora-easy-karma fedora-packager mediawriter lm_sensors bash-completion bash-doc dvipng libvirt\* qemu\* virt-manager accountsdialog gparted samba-client p7zip\* rpmdevtools nmap wireshark-gtk iperf indent powertop htop iotop bpython python-farsight python-crypto python-virtualenv python-virtualenvwrapper python-pip fedmsg\* gnome-\*fedmsg mediainfo-gui cloc $NO_DEVEL $SKIP_MISSING_PACKAGE
 
 ## Games
-if [ "$1" != "WORK" ]; then
-	dnf -y install springlobby extremetuxracer bzflag clanbomber liquidwar\* fortune-mod frozen-bubble neverball pinball pingus powermanga scorched3d supertux\* torcs\* tuxpuck\* tuxtype2 warmux\* xmoto\* xscorch openalchemist celestia stellarium 0ad teeworlds $NO_DEVEL $SKIP_MISSING_PACKAGE
-fi
+dnf -y install springlobby extremetuxracer bzflag clanbomber liquidwar\* fortune-mod frozen-bubble neverball pinball pingus powermanga scorched3d supertux\* torcs\* tuxpuck\* tuxtype2 warmux\* xmoto\* xscorch openalchemist celestia stellarium 0ad teeworlds $NO_DEVEL $SKIP_MISSING_PACKAGE
 
 ## Themes
 ## Alternate wallpapers from Fedora: _\*-backgrounds-extras-gnome gears-backgrounds neon-backgrounds leonidas-backgrounds-landscape_ packages
@@ -86,17 +84,9 @@ dnf install langpacks-en langpacks-$LANGUAGE
 plymouth-set-default-theme charge
 /usr/libexec/plymouth/plymouth-update-initrd
 
-systemctl disable packagekit packagekit-offline-update livesys livesys-late lvm2-monitor
-systemctl enable libvirtd
-
 # Kernel config
 echo "kernel.sysrq = 1
-vm.swappiness = 15
 " > /etc/sysctl.conf
-
-systemctl mask libvirtd.service
-systemctl mask ModemManager.service
-systemctl disable multipathd.service
 
 
 # Boot option (add acpi compatibility for backlight) and add theme
@@ -104,9 +94,6 @@ sed -i 's/^GRUB_CMDLINE_LINUX="\([^:]*\)"/GRUB_CMDLINE_LINUX="\1 acpi_backlight=
 sed -i 's/GRUB_TERMINAL_OUTPUT="console"/#GRUB_TERMINAL_OUTPUT="console"/' /etc/default/grub
 echo "GRUB_THEME=\"/boot/grub2/themes/system/theme.txt\"" >> /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
-
-systemctl mask packagekit.service
-systemctl disable lvm2-monitor livesys.service
 
 if [ "$1" == "WORK" ]; then
 	# Enable NFS 2 and 4.1
@@ -122,12 +109,6 @@ if [ "$1" == "WORK" ]; then
 	firewall-cmd --permanent --zone=FedoraWorkstation --add-port=59962/tcp
 	firewall-cmd --reload
 
-	echo "[Service]
-Environment=\"NSS_HASH_ALG_SUPPORT=+MD5\"
-Environment=\"OPENSSL_ENABLE_MD5_VERIFY=1\"" > /etc/systemd/system/NetworkManager.service.d/override.conf
-
-	systemctl daemon-reload
-	systemctl restart NetworkManager.service
 	systemctl enable fstrim.timer
 	systemctl start fstrim.timer
 fi
